519676445b63f9d86e74d4b06546e3f1d89786b4
Scott Keller <scott.keller2390@gmail.com>
2018-01-08 10:49:12 -0500

    Fixed pathing issue with input files for game of life and minesweeper. Tests can now be ran from any directory

--- test/game_of_life_test.py
+++ test/game_of_life_test.py
@@ -10,22 +10,22 @@ from src.game_of_life import Game
 class TestGameOfLife(unittest.TestCase):
 
     def test_get_input_filename(self):
-        infile = os.path.abspath("./game_of_life_test_input/one_live_cell.txt")
+        infile = os.path.join(os.path.dirname(__file__), 'game_of_life_test_input/one_live_cell.txt')
         game = Game(infile)
         assert game.infile == infile
 
     def test_read_input_file(self):
-        infile = os.path.abspath("./game_of_life_test_input/one_live_cell.txt")
+        infile = os.path.join(os.path.dirname(__file__), 'game_of_life_test_input/one_live_cell.txt')
         game = Game(infile)
-        assert game.raw_input[0] == "*"
+        assert game.raw_input[0] == '*'
 
     def test_grid_creation(self):
-        infile = os.path.abspath("./game_of_life_test_input/one_live_cell.txt")
+        infile = os.path.join(os.path.dirname(__file__), 'game_of_life_test_input/one_live_cell.txt')
         game = Game(infile)
         assert game.grid == set([(0, 0, '*')])
 
     def test_live_cells(self):
-        infile = os.path.abspath("./game_of_life_test_input/one_live_cell.txt")
+        infile = os.path.join(os.path.dirname(__file__), 'game_of_life_test_input/one_live_cell.txt')
         game = Game(infile)
         assert game.live_cells == [(0, 0)]
 
@@ -35,36 +35,36 @@ class TestGameOfLife(unittest.TestCase):
         assert neighbors == [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
 
     def test_one_live_cell_death(self):
-        infile = os.path.abspath("./game_of_life_test_input/one_live_cell.txt")
+        infile = os.path.join(os.path.dirname(__file__), 'game_of_life_test_input/one_live_cell.txt')
         game = Game(infile)
         game.evolve()
         assert game.grid == set([(0, 0, '.')])
 
     def test_one_dead_cell_life(self):
-        infile = os.path.abspath("./game_of_life_test_input/one_dead_cell_life.txt")
+        infile = os.path.join(os.path.dirname(__file__), 'game_of_life_test_input/one_dead_cell_life.txt')
         game = Game(infile)
         game.evolve()
         assert game.grid == set([(0, 0, '*'), (0, 1, '*'), (1, 0, '*'), (1, 1, '*')])
 
     def test_grid_text(self):
         from src.game_of_life import grid_text
-        infile = os.path.abspath("./game_of_life_test_input/one_dead_cell_life.txt")
-        with open(infile, "r") as my_file:
+        infile = os.path.join(os.path.dirname(__file__), 'game_of_life_test_input/one_dead_cell_life.txt')
+        with open(infile, 'r') as my_file:
             file_text = my_file.read()
         game = Game(infile)
         assert file_text == grid_text(game.grid)
 
     def test_blinker(self):
         # blinker changes back and forth each evolution
-        infile = os.path.abspath("./game_of_life_test_input/blinker.txt")
+        infile = os.path.join(os.path.dirname(__file__), 'game_of_life_test_input/blinker.txt')
         game = Game(infile)
-        grid1 = set([(0, 0, "."), (1, 0, "."), (2, 0, "."),
-                     (0, 1, "*"), (1, 1, "*"), (2, 1, "*"),
-                     (0, 2, "."), (1, 2, "."), (2, 2, ".")])
+        grid1 = set([(0, 0, '.'), (1, 0, '.'), (2, 0, '.'),
+                     (0, 1, '*'), (1, 1, '*'), (2, 1, '*'),
+                     (0, 2, '.'), (1, 2, '.'), (2, 2, '.')])
 
-        grid2 = set([(0, 0, "."), (1, 0, "*"), (2, 0, "."),
-                     (0, 1, "."), (1, 1, "*"), (2, 1, "."),
-                     (0, 2, "."), (1, 2, "*"), (2, 2, ".")])
+        grid2 = set([(0, 0, '.'), (1, 0, '*'), (2, 0, '.'),
+                     (0, 1, '.'), (1, 1, '*'), (2, 1, '.'),
+                     (0, 2, '.'), (1, 2, '*'), (2, 2, '.')])
 
         assert game.grid == grid1
         game.evolve()
@@ -76,7 +76,7 @@ class TestGameOfLife(unittest.TestCase):
 
     def test_boat(self):
         # boat is a stable pattern
-        infile = os.path.abspath("./game_of_life_test_input/boat.txt")
+        infile = os.path.join(os.path.dirname(__file__), 'game_of_life_test_input/boat.txt')
         game = Game(infile)
         temp_grid = game.grid
         for i in range(1000):
@@ -85,7 +85,7 @@ class TestGameOfLife(unittest.TestCase):
 
     def test_extinction(self):
         # pattern will end in extinction after 2 evolutions
-        infile = os.path.abspath("./game_of_life_test_input/extinction.txt")
+        infile = os.path.join(os.path.dirname(__file__), 'game_of_life_test_input/extinction.txt')
         game = Game(infile)
         game.evolve()
         game.evolve()
--- test/minesweeper_test.py
+++ test/minesweeper_test.py
@@ -9,22 +9,22 @@ from src.minesweeper import Game
 
 class TestMinesweeper(unittest.TestCase):
     def test_get_input_filename(self):
-        infile = os.path.abspath('./minesweeper_test_input/one_field.txt')
+        infile = os.path.join(os.path.dirname(__file__), 'minesweeper_test_input/one_field.txt')
         game = Game(infile)
         game.infile = infile
 
     def test_get_raw_input(self):
-        infile = os.path.abspath('./minesweeper_test_input/one_field.txt')
+        infile = os.path.join(os.path.dirname(__file__), 'minesweeper_test_input/one_field.txt')
         game = Game(infile)
-        assert game.raw_input == ["."]
+        assert game.raw_input == ['.']
 
     def test_grid_creation(self):
-        infile = os.path.abspath('./minesweeper_test_input/one_field.txt')
+        infile = os.path.join(os.path.dirname(__file__), 'minesweeper_test_input/one_field.txt')
         game = Game(infile)
-        assert game.grid == set([(0, 0, ".")])
+        assert game.grid == set([(0, 0, '.')])
 
     def test_mines(self):
-        infile = os.path.abspath('./minesweeper_test_input/one_mine.txt')
+        infile = os.path.join(os.path.dirname(__file__), 'minesweeper_test_input/one_mine.txt')
         game = Game(infile)
         assert game.mines == [(0, 0)]
 
@@ -34,20 +34,20 @@ class TestMinesweeper(unittest.TestCase):
         assert neighbors == [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
 
     def test_hint_count(self):
-        infile = os.path.abspath('./minesweeper_test_input/one_mine.txt')
+        infile = os.path.join(os.path.dirname(__file__), 'minesweeper_test_input/one_mine.txt')
         game = Game(infile)
         assert game.hint() == set([(0, 0, '*'), (1, 0, 1)])
 
     def test_grid_text(self):
         from src.minesweeper import grid_text
-        infile = os.path.abspath("./minesweeper_test_input/4_4_grid_2_mines.txt")
-        with open(infile, "r") as my_file:
+        infile = os.path.join(os.path.dirname(__file__), 'minesweeper_test_input/4_4_grid_2_mines.txt')
+        with open(infile, 'r') as my_file:
             file_text = my_file.read()
         game = Game(infile)
         assert file_text == grid_text(game.grid)
 
     def test_4_4_grid_2_mines(self):
-        infile = os.path.abspath('./minesweeper_test_input/4_4_grid_2_mines.txt')
+        infile = os.path.join(os.path.dirname(__file__), 'minesweeper_test_input/4_4_grid_2_mines.txt')
         game = Game(infile)
         assert game.hint() == set([(0, 0, '*'), (1, 0, 1), (2, 0, 0), (3, 0, 0),
                                    (0, 1, 2), (1, 1, 2), (2, 1, 1), (3, 1, 0),
@@ -55,18 +55,18 @@ class TestMinesweeper(unittest.TestCase):
                                    (0, 3, 1), (1, 3, 1), (2, 3, 1), (3, 3, 0)])
 
     def test_3_5_grid_3_mines(self):
-        infile = os.path.abspath('./minesweeper_test_input/3_5_grid_3_mines.txt')
+        infile = os.path.join(os.path.dirname(__file__), 'minesweeper_test_input/3_5_grid_3_mines.txt')
         game = Game(infile)
         assert game.hint() == set([(0, 0, '*'), (1, 0, '*'), (2, 0, 1), (3, 0, 0), (4, 0, 0),
                                    (0, 1, 3), (1, 1, 3), (2, 1, 2), (3, 1, 0), (4, 1, 0),
                                    (0, 2, 1), (1, 2, '*'), (2, 2, 1), (3, 2, 0), (4, 2, 0)])
 
     def test_show_grid(self):
-        infile = os.path.abspath('./minesweeper_test_input/4_4_grid_2_mines.txt')
+        infile = os.path.join(os.path.dirname(__file__), 'minesweeper_test_input/4_4_grid_2_mines.txt')
         game = Game(infile)
         game.show_grid()
         game.show_hint()
-        infile = os.path.abspath('./minesweeper_test_input/3_5_grid_3_mines.txt')
+        infile = os.path.join(os.path.dirname(__file__), 'minesweeper_test_input/3_5_grid_3_mines.txt')
         game = Game(infile)
         game.show_grid()
         game.show_hint()
